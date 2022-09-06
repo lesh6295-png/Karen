080b276e393b7aa04664123fbd937dc387ad1a03
perdub <aleksejspakovskij968@gmail.com>
2022-09-06 22:42:36 +0300

    add main application

---
+++ Karen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Karen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.File.WriteAllText("message.txt", "HI!");
+        }
+    }
+}
