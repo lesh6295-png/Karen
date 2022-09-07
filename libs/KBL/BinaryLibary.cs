8ed910eb2e30593d4953ba9d26678d0c77a0a842
perdub <aleksejspakovskij968@gmail.com>
2022-09-07 23:26:18 +0300

    changes in `BinaryLibary`

--- libs/KBL/BinaryLibary.cs
+++ libs/KBL/BinaryLibary.cs
@@ -20,21 +20,38 @@ namespace Karen.KBL
             file = File.Open(libaryPath, FileMode.Open);
             ReadHeader();
         }
+        internal BinaryLibary(string libaryPath, int countOfFiles)
+        {
 
+        }
         private void ReadHeader()
         {
-            //Read count of files in libary
+            //Read count of files and libary id 
+            byte[] idbin = new byte[4];
+            file.Read(idbin, 0, 4);
+            LibaryId = BitConverter.ToInt32(idbin, 0);
             byte[] countbin = new byte[4];
             file.Read(countbin, 0, 4);
             Count = BitConverter.ToInt32(countbin, 0);
-
+            headerLength += 8;
             List<BinaryItem> items = new List<BinaryItem>();
             for(int i = 0; i < Count; i++)
             {
                 byte[] item = new byte[20];
                 file.Read(item, 0, 20);
-
+                BinaryItem q = new();
+                q.address = BitConverter.ToInt64(item, 0);
+                q.length = BitConverter.ToInt64(item, 7);
+                q.id = BitConverter.ToInt32(item, 16);
+                items.Add(q);
+                headerLength += 20;
             }
+            this.items = items.ToArray();
+        }
+
+        ~BinaryLibary()
+        {
+            file.Close();
         }
     }
     /// <summary>
@@ -42,7 +59,28 @@ namespace Karen.KBL
     /// </summary>
     public class BinaryItem
     {
-        public long Addres = 0, Length = 0;
-        public int Id=0;
+        internal long address = 0, length = 0;
+        internal int id=0;
+        public long Address
+        {
+            get
+            {
+                return address;
+            }
+        }
+        public long Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+        }
     }
 }
