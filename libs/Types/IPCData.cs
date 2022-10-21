4ed9eca835915149ee5c18af7ab6cf28f2aa9a9f
perdub <aleksejspakovskij968@gmail.com>
2022-10-21 22:50:02 +0300

    Add IPCData class
    Shit, now i dont want used libary to interprocessing, so many time spend by this...

---
+++ libs/Types/IPCData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.Json;
+namespace Karen.Types
+{
+    /// <summary>
+    /// This class used to interprocess interactive.
+    /// </summary>
+    public class IPCData
+    {
+        public string Type { get; private set; }
+        public object FirstParam { get; private set; }
+        public object SecondParam { get; private set; }
+
+        public override string ToString()
+        {
+            JsonSerializerOptions op = new JsonSerializerOptions();
+#if DEBUG
+            op.WriteIndented = true;
+#endif
+            return JsonSerializer.Serialize<IPCData>(this);
+        }
+
+        public static IPCData FromString(string source)
+        {
+            return JsonSerializer.Deserialize<IPCData>(source);
+        }
+    }
+}
