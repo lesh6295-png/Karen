e5dd1cf90c72f4eed1812c2b12627b22fded3728
perdub <aleksejspakovskij968@gmail.com>
2022-11-05 18:38:48 +0300

    Edit tests.yml

--- game/Karen/Engine/Logger.cs
+++ game/Karen/Engine/Logger.cs
@@ -41,7 +41,7 @@ namespace Karen.Engine
             Exception e = (Exception)args.ExceptionObject;
             Write(e.Message);
 #if TESTING
-            File.WriteAllText(errorlogname,e.ToString());
+            File.WriteAllText(errorlogname, e.ToString());
 #else
             File.WriteAllText(dirname + "/" + errorlogname, e.ToString());
 #endif
