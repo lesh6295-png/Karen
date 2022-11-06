ad92c055ca98ecc3ed9255103b64fa93947505e4
perdub <aleksejspakovskij968@gmail.com>
2022-11-06 22:29:28 +0300

    Try to test events

--- game/Karen/Engine/Api/Api.cs
+++ game/Karen/Engine/Api/Api.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Karen.KBL;
 using Karen.Locale;
 using Karen.Engine.Scripting;
 using Karen.Types;
@@ -15,12 +15,9 @@ namespace Karen.Engine.Api
         public static async Task newsc(object?[]? par)
         {
             ScriptContext nsc = new ScriptContext((VirtualMachine)(((object[])par.Last())[2]));
-            if(par[0] is string q)
-            {
-                nsc.LoadScriptFromDrive(q);
+            nsc.LoadScriptFromByteArray(BinaryManager.Extract(Convert.ToInt32((string)par[0]), Convert.ToInt32((string)par[1])));
                 nsc.ExcecuteAsync();
             }
-        }
         public static async Task to(object?[]? par)
         {
             ((ScriptContext)(((object[])par.Last())[3])).ToLabel((string)par[0]);
--- game/Karen/Engine/Api/EventApi.cs
+++ game/Karen/Engine/Api/EventApi.cs
@@ -10,7 +10,15 @@ namespace Karen.Engine.Api
     {
         public static async Task waitevent(object?[]? par)
         {
-
+            await EventManager.Wait((string)par[0]);
+        }
+        public static async Task callevent(object?[]? par)
+        {
+            EventManager.CallEvent((string)par[0]);
+        }
+        public static async Task addevent(object?[]? par)
+        {
+            EventManager.AddEvent((string)par[0]);
         }
     }
 }
