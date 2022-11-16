bd21725a1c78b6bcd4d9637a989ae6f13d113b2b
perdub <aleksejspakovskij968@gmail.com>
2022-11-16 22:18:26 +0300

    Fix build error

--- game/Karen/Engine/Api/AssetsApi.cs
+++ game/Karen/Engine/Api/AssetsApi.cs
@@ -14,11 +14,11 @@ namespace Karen.Engine.Api
             {
                 case "load":
                     string path = par.TryExtractElement<object, string>("unk", 1);
-                    SourceManager.LoadSource(path);
+                    SourceManager.Singelton.LoadSource(path);
                     break;
                 case "unload":
                     int unloadid = par.TryExtractElement<object, int>(-1, 1);
-                    SourceManager.UnloadSource(unloadid);
+                    SourceManager.Singelton.UnloadSource(unloadid);
                     break;
                 default:
                     throw new InvalidApiParamsException("Unknown locales mode: " + mode);
