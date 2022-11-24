71b72cdd5f7f330030bf0567c283ae4b86a73ab7
perdub <aleksejspakovskij968@gmail.com>
2022-11-24 23:44:14 +0300

    Add Installer Core

---
+++ UI/Installer/Core/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<filesystem>
+#include<Windows.h>
+namespace fs = std::filesystem;
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	fs::path p = fs::current_path();
+	fs::create_directory(p / "temp");
+	fs::create_directory(p / "bin");
+	//CreateProcess(L"cmd", L"echo q > D:\\qwer", NULL, NULL, NULL, NORMAL_PRIORITY_CLASS, NULL, NULL, )
+    //WinExec(LPCSTR("cmd.exe mkdir pidor"), SW_HIDE);
+	return 0;
+}
