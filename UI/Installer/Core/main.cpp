6930a95eec849f0a9cdc289688d1e1124ba7d3b1
perdub <aleksejspakovskij968@gmail.com>
2022-11-26 11:20:57 +0300

    work on installer core

--- UI/Installer/Core/main.cpp
+++ UI/Installer/Core/main.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<filesystem>
 #include<Windows.h>
+#include<shellapi.h>
 namespace fs = std::filesystem;
-
+void cmd_execute(LPCWSTR command) {
+	ShellExecute(NULL, NULL, L"cmd.exe",command, NULL, SW_SHOWNORMAL);
+}
+void download_temp() {
+	cmd_execute(L"/k ");
+}
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	fs::path p = fs::current_path();
 	fs::create_directory(p / "temp");
 	fs::create_directory(p / "bin");
 
-	//CreateProcess(L"cmd", L"echo q > D:\\qwer", NULL, NULL, NULL, NORMAL_PRIORITY_CLASS, NULL, NULL, )
-    //WinExec(LPCSTR("cmd.exe mkdir pidor"), SW_HIDE);
+	bool localinst = !fs::exists(p / "1.bin");
+
+	download_temp();
+
 	return 0;
 }
