4ceeee15f93f6b9cd45b573a51673c742adba857
perdub <aleksejspakovskij968@gmail.com>
2022-11-27 16:22:44 +0300

    Stupid python

---
+++ UI/Installer/InstallerBuilder/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using Karen.Types;
+namespace Karen.InstallerBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string config = args[0];
+            string netver = args[1].Split('/').Last().Replace("\\","");
+            Console.WriteLine("Start directory: "+Environment.CurrentDirectory);
+            Environment.CurrentDirectory += "/../../../../../../bin";
+            //delete last build result
+            if(Directory.Exists("Installer"))
+                Directory.Delete("Installer", true);
+
+            string path = $"Installer/{config}/{netver}";
+            Directory.CreateDirectory(path+"/offline");
+            Directory.CreateDirectory(path + "/local");
+            Directory.CreateDirectory(path + "/minimal");
+
+            //copy installer.exe
+            File.Copy("installer-core/" + config + "/installer.exe", path + "/offline/installer.exe");
+            File.Copy("installer-core/" + config + "/installer.exe", path + "/local/installer.exe");
+            File.Copy("installer-core/" + config + "/installer.exe", path + "/minimal/installer.exe");
+
+            //copy 1.bin
+            File.Copy("InstallerTemp/" + config + "/"+netver+"/installer.zip", path + "/offline/1.bin");
+            File.Copy("InstallerTemp/" + config + "/" + netver + "/installer.zip", path + "/local/1.bin");
+
+            //todo: add copy raw karen to offline build
+
+
+            //write config
+            string globalr = Karen.Types.Extensions.RandomString(min: 10, max: 20).ToHex();
+            File.WriteAllText(path + "/config.ini", globalr);
+            File.WriteAllText(path + "/offline/config.ini", globalr);
+            File.WriteAllText(path + "/local/config.ini", globalr);
+            File.WriteAllText(path + "/minimal/config.ini", globalr);
+        }
+    }
+}
