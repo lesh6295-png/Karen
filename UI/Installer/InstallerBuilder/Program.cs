00dc765c8f835b9986b6c5c64aff0439c6c9d3ef
perdub <aleksejspakovskij968@gmail.com>
2022-12-02 17:47:24 +0300

    Work on lnstallerBuilder

--- UI/Installer/InstallerBuilder/Program.cs
+++ UI/Installer/InstallerBuilder/Program.cs
@@ -9,7 +9,7 @@ namespace Karen.InstallerBuilder
         static void Main(string[] args)
         {
             string config = args[0];
-            string netver = args[1].Split('/').Last().Replace("\\","");
+           // string netver = args[1].Split('/').Last().Replace("\\","");
             Console.WriteLine("Start directory: "+Environment.CurrentDirectory);
             Environment.CurrentDirectory += "/../../../../../../bin";
             //delete last build result
@@ -17,34 +17,72 @@ namespace Karen.InstallerBuilder
             {
                 if (Directory.Exists("Installer"))
                     Directory.Delete("Installer", true);
+                Directory.CreateDirectory("Installer");
             }
             catch
             {
 
             }
-            string path = $"Installer/{config}/{netver}";
-            Directory.CreateDirectory(path+"/offline");
-            Directory.CreateDirectory(path + "/local");
-            Directory.CreateDirectory(path + "/minimal");
+            //copy Karen archives binarys
+            string[] karenbins = Directory.GetFiles("Karen/", "game.zip", SearchOption.AllDirectories);
+            foreach(var q in karenbins)
+            {
+                string shortname = "";
+                if (q.Contains("Debug"))
+                {
+                    shortname += "d";
+                }
+                else if (q.Contains("Testing"))
+                {
+                    shortname += "t";
+                }
+                else if (q.Contains("Release"))
+                {
+                    shortname += "r";
+                }
+
+                if (q.Contains("5"))
+                {
+                    shortname += "5";
+                }
+                else if (q.Contains("6"))
+                {
+                    shortname += "6";
+                }
+                else if (q.Contains("7"))
+                {
+                    shortname += "7";
+                }
+
+                shortname += ".bin";
+                File.Move(q, $"Installer/{shortname}", true);
+            }
 
             //copy installer.exe
-            File.Copy("installer-core/" + config + "/installer.exe", path + "/offline/installer.exe");
-            File.Copy("installer-core/" + config + "/installer.exe", path + "/local/installer.exe");
-            File.Copy("installer-core/" + config + "/installer.exe", path + "/minimal/installer.exe");
+            File.Copy($"installer-core/{config}/installer.exe", "Installer/installer.exe", true);
+            //string path = $"Installer/{config}/{netver}";
+            //Directory.CreateDirectory(path+"/offline");
+            //Directory.CreateDirectory(path + "/local");
+            //Directory.CreateDirectory(path + "/minimal");
 
-            //copy 1.bin
-            File.Copy("InstallerTemp/" + config + "/"+netver+"/installer.zip", path + "/offline/1.bin");
-            File.Copy("InstallerTemp/" + config + "/" + netver + "/installer.zip", path + "/local/1.bin");
+            ////copy installer.exe
+            //File.Copy("installer-core/" + config + "/installer.exe", path + "/offline/installer.exe");
+            //File.Copy("installer-core/" + config + "/installer.exe", path + "/local/installer.exe");
+            //File.Copy("installer-core/" + config + "/installer.exe", path + "/minimal/installer.exe");
 
-            //todo: add copy raw karen to offline build
+            ////copy 1.bin
+            //File.Copy("InstallerTemp/" + config + "/"+netver+"/installer.zip", path + "/offline/1.bin");
+            //File.Copy("InstallerTemp/" + config + "/" + netver + "/installer.zip", path + "/local/1.bin");
+
+            ////todo: add copy raw karen to offline build
 
 
-            //write config
-            string globalr = Karen.Types.Extensions.RandomString(min: 10, max: 20).ToHex();
-            File.WriteAllText(path + "/config.ini", globalr);
-            File.WriteAllText(path + "/offline/config.ini", globalr);
-            File.WriteAllText(path + "/local/config.ini", globalr);
-            File.WriteAllText(path + "/minimal/config.ini", globalr);
+            ////write config
+            //string globalr = Karen.Types.Extensions.RandomString(min: 10, max: 20).ToHex();
+            //File.WriteAllText(path + "/config.ini", globalr);
+            //File.WriteAllText(path + "/offline/config.ini", globalr);
+            //File.WriteAllText(path + "/local/config.ini", globalr);
+            //File.WriteAllText(path + "/minimal/config.ini", globalr);
         }
     }
 }
