9980f69f9a5c60ed39713023ca12998d37e719aa
lesh6295-png <my.emailtounity@gmail.com>
2022-12-27 22:55:04 +0300

    Add method to convert int to 36-char system(я не знаю как написать название системы на английском, но если вы можете это прочитать, функция переводит int в 36-чную систему, которая хранится в string)

---
+++ libs/CodeOptimizator/NameShorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeOptimizator
+{
+    public class NameShorter
+    {
+        static readonly char[] s36 = "0123456789abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        static char get_36_sys_symb(int i)
+        {
+            return s36[i];
+        }
+        public static string get36sys(int value)
+        {
+            int nc = Convert.ToInt32(Math.Round(Math.Log(value+1, 36), MidpointRounding.ToPositiveInfinity));
+            int nd = value, i = 0;
+            char[] r = new char[nc];
+            while (true)
+            {
+                int d = nd % 36;
+                r[nc - i -1] = get_36_sys_symb(d);
+                nd = (nd - d) / 36;
+                if (i+1>=nc)
+                    break;
+                i++;
+            }
+            StringBuilder b = new();
+            b.Append(r);
+            return b.ToString();
+        }
+    }
+}
--- libs/Tester/Program.cs
+++ libs/Tester/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
+
 namespace Tester
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Karen.Locale.KeySourceFactory.GenerateTestSource("test.locale");
-            var q = new Karen.Locale.KeySource("2.locale");
-            Console.WriteLine(Karen.Locale.Localization.Culture);
-            Console.WriteLine(q.TryExtractTranslate("karen_clothes"));
+            Console.WriteLine(CodeOptimizator.NameShorter.get36sys(Convert.ToInt32(Console.ReadLine())));
             Console.ReadKey();
         }
     }
